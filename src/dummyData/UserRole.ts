643492d57eb74a11a6b72c156c724ecd862f0f89
mestrynarayan <narayan.mestry@infogen-labs.com>
2024-02-06 14:16:36 +0530

    add educatore form completed with validation

---
+++ src/dummyData/UserRole.ts
@@ -0,0 +1,19 @@
+import { ISelectItemList } from '../Interface/ISelectItemList'
+const UserRole: ISelectItemList[] = [
+    {
+        'id': 1,
+        'labelName': 'Teacher',
+        'value': 'Teacher'
+    },
+    {
+        'id': 2,
+        'labelName': 'School Admin',
+        'value': 'School Admin'
+    },
+    {
+        'id': 3,
+        'labelName': 'Organization Admin',
+        'value': 'Organization Admin'
+    }
+]
+export default UserRole
--- src/utils/AllRoutes.tsx
+++ src/utils/AllRoutes.tsx
@@ -7,6 +7,7 @@ import IgniteChildPLayTime from '../pages/orgadmin/IgniteChildPLayTime'
 import OrganizationAdminDashboard from "../pages/orgadmin/OrganizationAdminDashboard";
 import ViewSchool from "../pages/orgadmin/ViewSchool";
 import AddClass from "../pages/orgadmin/addClass/AddClass";
+import AddEducator from "../pages/orgadmin/addEducator/AddEducator";
 
 function AllRoutes() {
     return (
@@ -14,12 +15,13 @@ function AllRoutes() {
             <Route path="/" element={<LoginRegistrationPage />} />
             <Route path="org" element={<RootLayout />}>
                 {/* Org Admin Routes */}
-                <Route index element={<OrganizationAdminDashboard/>}/>   
+                <Route index element={<OrganizationAdminDashboard />} />
                 <Route path="view-children" element={<ViewChildren />} />
                 <Route path="view-school" element={<ViewSchool />} />
                 <Route path="add-child" element={<AddChild />} />
                 <Route path="ignite-child-play-time" element={<IgniteChildPLayTime />} />
-                <Route path="add-class" element={<AddClass/>}/>   
+                <Route path="add-class" element={<AddClass />} />
+                <Route path="add-educator" element={<AddEducator />} />
             </Route>
         </Routes>
     )
